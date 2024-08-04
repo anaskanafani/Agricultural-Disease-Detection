75c3f1e10e28884711a1ffad0ddd216e3f56e948
anaskanafani <anas.kanafani10@outlook.com>
2024-08-05 00:51:28 +0400

    fix: fix heroku deployment issues #12

--- api/main.py
+++ api/main.py
@@ -8,6 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Add CORS middleware
 origins = [
     "http://localhost:3000",
     "https://tomato-disease-cnn.vercel.app",
