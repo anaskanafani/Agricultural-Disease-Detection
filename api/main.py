7f12f1adc766139ccc31897a422f27fc9451602b
anaskanafani <anas.kanafani10@outlook.com>
2024-08-04 22:11:57 +0400

    feat: add api

---
+++ api/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+model = tf.keras.models.load_model('tomato_disese_model_V1.keras')
+
+class_names = ['Bacterial_spot', 'Healthy', 'Septoria_leaf_spot',
+               'Spider_mites_Two_spotted_spider_mite', 'YellowLeaf__Curl_Virus']
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(BytesIO(contents))
+    image = np.array(image)
+    image = np.expand_dims(image, axis=0)
+    prediction = model.predict(image)
+    predicted_class = class_names[np.argmax(prediction)]
+    return {"class": predicted_class, "confidence": str(np.max(prediction))}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
