36a5514153cb4b0b0fa50dc4bff78156b9873b6d
anaskanafani <anas.kanafani10@outlook.com>
2024-08-04 22:22:37 +0400

    feat: allow cors on dev and prod urls

--- api/main.py
+++ api/main.py
@@ -4,8 +4,22 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://tomato-disease-cnn.vercel.app/",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 model = tf.keras.models.load_model('tomato_disese_model_V1.keras')
 
@@ -25,7 +39,7 @@ async def predict(file: UploadFile = File(...)):
     image = np.expand_dims(image, axis=0)
     prediction = model.predict(image)
     predicted_class = class_names[np.argmax(prediction)]
-    return {"class": predicted_class, "confidence": str(np.max(prediction))}
+    return {"class": predicted_class, "confidence": str(np.max(prediction) * 100) + "%"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
